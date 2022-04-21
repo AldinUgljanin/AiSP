35c82520d557cac373d3f8dfea866c823d8dce04
AldinUgljanin <aldinugljanin@protonmail.com>
2022-04-21 17:29:27 +0200

    better algorithm

--- linked_list/src/07_sorted_list.c
+++ linked_list/src/07_sorted_list.c
@@ -19,16 +19,16 @@ int main(){
 
     NODE *root = NULL;
     
-    append_element(&root, 10);
-    append_element(&root, 20);
-    insert_beginning(&root, 30);
-    insert_beginning(&root, 40);
+    append_element(&root, 30);
+    append_element(&root, 40);
+    insert_beginning(&root, 20);
+    insert_beginning(&root, 10);
 
     affter_element(&root, 40, 50);
     affter_element(&root, 20, 25);
     affter_element(&root, 30, 35);
 
-    insert_sorted(&root, 21);
+    insert_sorted(&root, 51);
     insert_sorted(&root, 26);
 
     print_list(&root);
@@ -48,7 +48,7 @@ void append_element(NODE **root, int value){
     end_node->data = value;
     end_node->next = NULL;
 
-    if(*root == NULL){
+    if(*root == NULL) {
 	*root = end_node;
 	return;
     }
@@ -93,7 +93,9 @@ void affter_element(NODE **root, int element, int value){
 
     NODE *new_node = malloc(sizeof(NODE));
     if(new_node == NULL) exit(4);
+
     new_node->data = value;
+    new_node->next = NULL;
 
     NODE *temp = *root;
     while(temp != NULL){
@@ -108,22 +110,20 @@ void affter_element(NODE **root, int element, int value){
 }
 
 void insert_sorted(NODE **root,int value){
-    NODE *new_node = malloc(sizeof(NODE));
-    if(new_node == NULL) exit(5);
-    new_node->data = value;
-    new_node->next = NULL;
+    if(*root == NULL || (*root)->data >= value) {
+	insert_beginning(root, value);
+	return;
+    }
 
     NODE *temp = *root;
-    while(temp != NULL){
-	if(new_node->data > temp->data && temp->next == NULL){
-	    new_node->next = temp->next;
-	    temp->next = new_node;
-	    break;
-	} else if(new_node->data > temp->data && new_node->data <= temp->next->data){
-	    new_node->next = temp->next;
-	    temp->next = new_node;
-	    break;
+    while(temp->next != NULL){
+	if(temp->next->data >= value){
+	    affter_element(root, temp->data, value);
+	    return;
 	}
 	temp = temp->next;
     }
+
+    affter_element(root, temp->data, value);
 }
+
